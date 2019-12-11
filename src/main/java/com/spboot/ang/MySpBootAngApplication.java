1ca9714199b964f081269f32cd2377c7d74edc63
uchachaduneli@gmail.com <anacuabanacua123%21%40%23>
2019-12-11 17:00:11 +0400

    first commit

---
+++ src/main/java/com/spboot/ang/MySpBootAngApplication.java
@@ -0,0 +1,13 @@
+package com.spboot.ang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpBootAngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpBootAngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spboot/ang/MySpBootAngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spboot.ang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpBootAngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
